f97ce3191f8ea317f990cc8dcc322cdfe03faeac
oguzozkaraca <oguzozkaraca@gmail.com>
2022-08-07 15:47:31 +0300

    spring admin server and ui

--- zipkin-server/src/main/java/com/ozkaraca/ticketproject/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/ozkaraca/ticketproject/ZipkinServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
-
 @SpringBootApplication
 public class ZipkinServerApplication {
 
